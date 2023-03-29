91f57321d07e51b9df9f01e867b1327644ad7c8d
K-Maxim <m.karpenko96@mail.ru>
2023-03-29 13:46:49 +0300

    Update

--- src/comment/model.py
+++ src/comment/model.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from src.core.db import Base
-import random
 
 from src.picture.model import Picture
 
@@ -10,7 +9,7 @@ class Comment(Base):
     __tablename__ = "comment"
 
     id = Column(Integer, primary_key=True, index=True, unique=True)
-    username = Column(String, default=f'Гость-{random.randrange(5)}', nullable=True)
+    username = Column(String)
     comment = Column(String)
     picture = Column(Integer, ForeignKey('picture.id', ondelete="CASCADE"))
     picture_id = relationship(Picture)
